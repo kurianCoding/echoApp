1aa9c7ab39767a79e182335fa59ce5881f6ab3d6
kurian <kurianck@yahoo.in>
2018-06-25 07:34:10 +0530

    Do Login     Added user model with login, method

--- app.go
+++ app.go
@@ -2,8 +2,15 @@ package main
 
 func main() {
 	//TODO: write a router init
+	router := echo.New()
+	router.Use(middleware.Recovery())
+	router.Use(middleware.Log())
+	router.GET("/status", func(c echo.Context) {
+		c.JSON(http.StatusOK, map[string]string{"health": "OK"})
+	})
 	//TODO: write a log init
 	//TODO: write a db connection init
 	//TODO: gracefully close the server with a defer functino
 	//TODO: run the server
+	router.Start(":8080")
 }
--- models/User.go
+++ models/User.go
@@ -1,3 +1,28 @@
 package models
 
 //TODO:define a function for login
+
+type User struct {
+	Name       string
+	Email      string
+	Password   string
+	DBPassword string
+}
+
+func DoLogin(user *User) (bool, error) {
+	//TODO: get User from database
+	user, err := GetUser(user.Name, user.Email)
+	if err != nil {
+		return false, err //false because user is not logged in
+	}
+	//TODO: check if passwords match
+	ok := bcrypt.CompareHashAndPassword([]byte(user.DBPassword), []byte(user.Password))
+	if !ok {
+		return false, nil
+	}
+	//TODO: return true if OK
+	return true, nil
+}
+func GetUser(name, email string) (*User, error) {
+
+}
